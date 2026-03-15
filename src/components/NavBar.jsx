885353c6e9238208f47873cbe0c656b5a5bf6790
zi-eunn <jieunkim1117@gmail.com>
2026-03-15 21:07:00 +0900

    feat: NavBar 불필요한 기능 삭제

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,8 +14,6 @@ const Navbar = () => {
             </div>
 
             <ul className={styles.navLinks}>
-                <li><Link to="/Compare">상품비교</Link></li>
-                <li><Link to="/Tips">금융꿀팁</Link></li>
 
                 <li><Link to="/My">마이페이지</Link></li>
                 {user ? (
